2898d9d2043eec371c0fd4ca62e86a22755f4793
bamchoh <bamchoh@gmail.com>
2018-04-13 00:54:31 +0900

    Reuse src array not to use temporary memory as possible

--- heapSort.go
+++ heapSort.go
@@ -1,58 +1,51 @@
 package main
 
-func upheap(a []int, n int) []int {
+func upheap(a *[]int, n int) {
 	if n < 1 {
-		return a
+		return
 	}
 	pn := (n+1)/2 - 1
-	if a[pn] > a[n] {
-		a[pn], a[n] = a[n], a[pn]
-		a = upheap(a, pn)
+	if (*a)[pn] < (*a)[n] {
+		(*a)[pn], (*a)[n] = (*a)[n], (*a)[pn]
+		upheap(a, pn)
 	}
-	return a
 }
 
-func insert(a []int, v int) []int {
-	a = append(a, v)
-	return upheap(a, len(a)-1)
+func insert(a *[]int, n int) {
+	upheap(a, n)
 }
 
-func downheap(a []int, k int) []int {
-	v := a[k]
+func downheap(a *[]int, k int, n int) {
+	v := (*a)[k]
 	var j int
-	for ; k < len(a)/2; k = j {
+	for ; k < (n+1)/2; k = j {
 		j = 2*(k+1) - 1
-		if j < len(a)-1 && a[j] > a[j+1] {
+		if j <= n-1 && (*a)[j] < (*a)[j+1] {
 			j++
 		}
-		if v <= a[j] {
+		if v >= (*a)[j] {
 			break
 		}
-		a[k] = a[j]
+		(*a)[k] = (*a)[j]
 	}
-	a[k] = v
-	return a
+	(*a)[k] = v
 }
 
-func remove(a []int) (int, []int) {
-	v := a[0]
-	a[0] = a[len(a)-1]
-	if len(a) == 1 {
-		return v, make([]int, 0)
+func remove(a *[]int, n int) {
+	if n == 0 {
+		return
 	}
-	a = downheap(a[0:len(a)-1], 0)
-	return v, a
+
+	(*a)[0], (*a)[n] = (*a)[n], (*a)[0]
+	downheap(a, 0, n-1)
 }
 
 func heapSort(src []int) []int {
-	var a []int
-	for _, v := range src {
-		a = insert(a, v)
+	for i := 0; i < len(src); i++ {
+		insert(&src, i)
 	}
-	var v int
-	for i := 0; len(a) != 0; i++ {
-		v, a = remove(a)
-		src[i] = v
+	for i := len(src) - 1; i >= 0; i-- {
+		remove(&src, i)
 	}
 	return src
 }
